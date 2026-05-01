5bcbd24c8028e0efa8c00f910e38f4b48aaff971
RATNA2005 <ratnakhanna2005@gmail.com>
2026-05-01 20:05:19 +0530

    Added lab2 stock price

---
+++ lab2-stock price prediction/StockPricePrediction.py
@@ -0,0 +1,28 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sb
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
+from xgboost import XGBClassifier
+from sklearn import metrics
+
+import warnings
+warnings.filterwarnings('ignore')
+
+df = pd.read_csv('Tesla.csv')
+print(df.head())
+print(df.shape)
+print(df.describe())
+print(df.info())
+#eda
+plt.figure(figsize=(15,5))
+plt.plot(df['Close'], color='blue')
+plt.title('Tesla Close Price', fontsize=15)
+plt.xlabel('Date')
+plt.ylabel('Price in dollars')
+plt.grid(True)
+plt.show()
